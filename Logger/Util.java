fbf8e02a8c0a8ab577e83edf51e7ab1524f0ecad
stamie <estampel@gmail.com>
2019-01-11 19:48:05 +0100

    WorkMonth class test 11.

--- Logger/Util.java
+++ Logger/Util.java
@@ -7,6 +7,7 @@ package Logger;
 
 import java.time.Duration;
 import java.time.LocalTime;
+import timelogger.exceptions.OwnException;
 
 /**
  *
@@ -14,22 +15,31 @@ import java.time.LocalTime;
  */
 public class Util {
 
-    public boolean isMultipleQuarterHour(LocalTime startTime, LocalTime endTime) {
+    public boolean isMultipleQuarterHour(LocalTime startTime, LocalTime endTime) throws OwnException {
+        if (startTime == null || endTime == null) {
+            throw new OwnException("EmptyTimeFieldException");
 
-        long minutes = Duration.between(startTime, endTime).toMinutes();
+        } else if (startTime.isAfter(endTime)) {
+            throw new OwnException("NotExpectedTimeOrderException");
+        } else {
+            long minutes = Duration.between(startTime, endTime).toMinutes();
 
-        if (minutes % 15 == 0 && minutes > 15) {
+            if (minutes % 15 == 0 && minutes >= 15) {
 
-            return true;
+                return true;
 
+            }
+
+            return false;
         }
-
-        return false;
 
     }
 
     public boolean isSeparatedTime(LocalTime startTime1, LocalTime endTime1, LocalTime startTime2, LocalTime endTime2) {
-        if (startTime1 == startTime2 || endTime2 == endTime1) {
+
+        if ((startTime1.equals(startTime2) || endTime2.equals(endTime1))
+                && (!startTime1.equals(endTime1) && !startTime2.equals(endTime2))) {
+
             return false;
         }
 
@@ -42,6 +52,21 @@ public class Util {
         }
 
         return true;
+    }
+
+    public LocalTime roundToMultipleQuarterHour(LocalTime startTime, LocalTime endTime) {
+
+        long mod = Duration.between(startTime, endTime).toMinutes() % 15;
+        LocalTime endTime2 = LocalTime.of(endTime.getHour(), endTime.getMinute());
+
+        if (mod < 15 - mod && Duration.between(startTime, endTime).toMinutes() - mod >= 15) {
+            endTime2 = endTime2.minusMinutes(mod);
+
+        } else {
+            endTime2 = endTime2.plusMinutes(15 - mod);
+
+        }
+        return endTime2;
     }
 
 }
